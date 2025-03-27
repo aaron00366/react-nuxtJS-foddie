1960c49e799a917655f28a52a3cf3e4a5cbb7bef
Aaron <aaron@foreknowledge.com.tw>
2025-03-27 14:30:24 +0800

    first time commit

---
+++ app/meals/[mealSlug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+    return (
+        <div className="not-found">
+            <h1>Meal not found</h1>
+            <p>Unfortunately, we could not find the requested page or requested meal data.</p>
+        </div>
+    );
+}
---
+++ app/meals/loading-out.js
@@ -0,0 +1,4 @@
+import classes from './MealsLoadingPage.module.css';
+export default function MealsLoadingPage() {
+    return <div className={ classes.loading }>Fetching meals ...</div>;
+}
